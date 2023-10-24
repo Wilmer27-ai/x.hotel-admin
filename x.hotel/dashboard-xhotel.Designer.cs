15c3a830ce4fc5a0586411d4db927e68230bcd42
NariszKhyll <145315327+NariszKhyll@users.noreply.github.com>
2023-10-24 13:51:24 +0800

    yiuy

--- x.hotel/dashboard-xhotel.Designer.cs
+++ x.hotel/dashboard-xhotel.Designer.cs
@@ -52,6 +52,7 @@ namespace x.hotel
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.button1.FlatAppearance.BorderSize = 20;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
@@ -60,16 +61,17 @@ namespace x.hotel
             this.button1.Size = new System.Drawing.Size(113, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "Calendar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.button2.Location = new System.Drawing.Point(954, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(113, 31);
             this.button2.TabIndex = 1;
             this.button2.Text = "Manage Rooms";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
@@ -83,8 +85,9 @@ namespace x.hotel
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.button4.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Location = new System.Drawing.Point(954, 137);
@@ -299,7 +302,6 @@ namespace x.hotel
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -314,5 +316,6 @@ namespace x.hotel
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button4;
     }
 }
