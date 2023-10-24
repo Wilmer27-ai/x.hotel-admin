c55a232a3012b8e33261d2e4cb53fe7f38e7f7d4
Wilmer Suelo <wilmer.suelo@wvsu.edu.ph>
2023-10-24 12:58:21 +0800

    lala

--- x.hotel/Form1.Designer.cs
+++ x.hotel/Form1.Designer.cs
@@ -42,7 +42,7 @@ namespace x.hotel
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(34, 109);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.Size = new System.Drawing.Size(73, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -52,7 +52,7 @@ namespace x.hotel
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(38, 174);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.Size = new System.Drawing.Size(69, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password";
             // 
@@ -61,7 +61,7 @@ namespace x.hotel
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(176, 47);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.Size = new System.Drawing.Size(69, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "X-HOTEL";
             // 
@@ -82,7 +82,7 @@ namespace x.hotel
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(179, 232);
+            this.button1.Location = new System.Drawing.Point(165, 224);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 36);
             this.button1.TabIndex = 5;
