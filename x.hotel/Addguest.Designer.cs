87934215df1f1e0e326215790f88e16e1025911f
NariszKhyll <nariszkhyll.binalber@wvsu.edu.ph>
2023-11-09 15:15:58 +0800

    ED

--- x.hotel/Addguest.Designer.cs
+++ x.hotel/Addguest.Designer.cs
@@ -41,18 +41,18 @@
             this.Rooms1.BackgroundColor = System.Drawing.Color.LightSkyBlue;
             this.Rooms1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.Rooms1.GridColor = System.Drawing.SystemColors.ActiveCaption;
-            this.Rooms1.Location = new System.Drawing.Point(386, 322);
+            this.Rooms1.Location = new System.Drawing.Point(71, 144);
             this.Rooms1.Name = "Rooms1";
             this.Rooms1.RowHeadersWidth = 51;
             this.Rooms1.RowTemplate.Height = 24;
-            this.Rooms1.Size = new System.Drawing.Size(1068, 294);
+            this.Rooms1.Size = new System.Drawing.Size(857, 294);
             this.Rooms1.TabIndex = 1;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(1213, 631);
+            this.button2.Location = new System.Drawing.Point(687, 459);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(122, 34);
             this.button2.TabIndex = 2;
@@ -62,7 +62,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(1341, 631);
+            this.button3.Location = new System.Drawing.Point(815, 459);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(113, 34);
             this.button3.TabIndex = 3;
@@ -72,12 +72,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(679, 211);
+            this.label1.Location = new System.Drawing.Point(368, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(496, 32);
+            this.label1.Size = new System.Drawing.Size(253, 32);
             this.label1.TabIndex = 4;
-            this.label1.Text = "CHECK B DZAI KUNG FULL SCREEN";
+            this.label1.Text = "WALK-IN GUESTS";
             // 
             // Addguest
             // 
@@ -85,7 +86,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.ClientSize = new System.Drawing.Size(1024, 546);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
