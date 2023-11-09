499c9e347bce9e21e542a749af273e2adce9f1f3
NariszKhyll <nariszkhyll.binalber@wvsu.edu.ph>
2023-11-09 15:09:28 +0800

    FDFDF

--- x.hotel/Addguest.Designer.cs
+++ x.hotel/Addguest.Designer.cs
@@ -32,6 +32,7 @@
             this.Rooms1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Rooms1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,7 +41,7 @@
             this.Rooms1.BackgroundColor = System.Drawing.Color.LightSkyBlue;
             this.Rooms1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.Rooms1.GridColor = System.Drawing.SystemColors.ActiveCaption;
-            this.Rooms1.Location = new System.Drawing.Point(50, 102);
+            this.Rooms1.Location = new System.Drawing.Point(386, 322);
             this.Rooms1.Name = "Rooms1";
             this.Rooms1.RowHeadersWidth = 51;
             this.Rooms1.RowTemplate.Height = 24;
@@ -50,8 +51,8 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(877, 411);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1213, 631);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(122, 34);
             this.button2.TabIndex = 2;
@@ -61,12 +62,22 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(1005, 411);
+            this.button3.Location = new System.Drawing.Point(1341, 631);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(113, 34);
             this.button3.TabIndex = 3;
             this.button3.Text = "Cancel";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(679, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(496, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CHECK B DZAI KUNG FULL SCREEN";
             // 
             // Addguest
             // 
@@ -74,7 +85,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(1176, 490);
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.Rooms1);
@@ -84,6 +96,7 @@
             this.Load += new System.EventHandler(this.Addguest_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Rooms1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -91,5 +104,6 @@
         private System.Windows.Forms.DataGridView Rooms1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
