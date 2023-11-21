ab48ac3ac7e31e4bb4cd5021c3dfe7b8bf1128b5
NariszKhyll <nariszkhyll.binalber@wvsu.edu.ph>
2023-11-21 13:58:10 +0800

    edited onboarding & signup form

--- x.hotel/Onboarding.Designer.cs
+++ x.hotel/Onboarding.Designer.cs
@@ -31,7 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Onboarding));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -55,15 +54,6 @@
             this.label2.Size = new System.Drawing.Size(0, 95);
             this.label2.TabIndex = 1;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -74,7 +64,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(359, 89);
             this.button2.TabIndex = 3;
-            this.button2.Text = "SIGN IN";
+            this.button2.Text = "PROCEED";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // Onboarding
@@ -85,7 +75,6 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1086, 624);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
@@ -100,7 +89,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
 }
